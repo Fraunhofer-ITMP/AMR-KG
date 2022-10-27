ed8a8794380a85d5afa62a180c5772ee48893adc
YojanaGadiya <yojanagadiya@gmail.com>
2022-10-27 12:36:29 +0200

    add script to pull latest data from ChEMBL

---
+++ src/get_chembl_data.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+"""Source code to get latest data from ChEMBL."""
+
+import chembl_downloader
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
+
+def get_data():
+    """Method to get AMR related data from ChEMBL."""
+
+    bacterial_species = pd.read_csv(f'../data/AMR/pathogen.csv')['pathogen'].to_list()
+
+    version, path = chembl_downloader.download_extract_sqlite(return_version=True)
+
+    get_assays = """
+    SELECT
+        ASSAYS.assay_id as id,
+        ASSAYS.assay_organism,
+    FROM ASSAYS
+    WHERE ASSAYS.assay_type == 'F'
+    """
+
+    assay_df = chembl_downloader.query(get_assays)
+    assay_df = assay_df[assay_df['assay_organism'].isin(bacterial_species)]
+
+    get_activity = """
+    SELECT
+        ACTIVITIES.activity_id,
+        ACTIVITIES.assay_id as id,
+        ACTIVITIES.standard_relation,
+        ACTIVITIES.standard_type,
+        ACTIVITIES.standard_value,
+        ACTIVITIES.standard_units,
+        ACTIVITIES.molregno as mol_id
+    FROM ACTIVITIES
+    """
+
+    act_df = chembl_downloader.query(get_activity)
+
+    data_df = pd.merge(assay_df, act_df, on='id', how='left')
+
+    get_compounds = """
+        SELECT
+            MOLECULE_DICTIONARY.molregno as mol_id,
+            MOLECULE_DICTIONARY.pref_name,
+            MOLECULE_DICTIONARY.max_phase
+        FROM MOLECULE_DICTIONARY
+        """
+
+    chem_df = chembl_downloader.query(get_compounds)
+
+    data_df = pd.merge(data_df, chem_df, on='mol_id', how='left')
+    data_df = data_df[data_df['standard_type'] == 'MIC']
+    data_df.to_csv(f'../data/MIC/data_dump_{version}.tsv', sep='\t', index=False)
+
+
+if __name__ == '__main__':
+    get_data()

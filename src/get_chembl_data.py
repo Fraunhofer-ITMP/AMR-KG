a97d16f30bb3532d5b405ce604fe2fcbda82c6b6
YojanaGadiya <yojanagadiya@gmail.com>
2022-10-28 09:41:32 +0200

    fix sql

--- src/get_chembl_data.py
+++ src/get_chembl_data.py
@@ -17,44 +17,52 @@ def get_data():
 
     get_assays = """
     SELECT
-        ASSAYS.assay_id as id,
-        ASSAYS.assay_organism,
-    FROM ASSAYS
-    WHERE ASSAYS.assay_type == 'F'
+            MOLECULE_DICTIONARY.pref_name,
+            MOLECULE_DICTIONARY.molregno as chembl_id,
+            ACTIVITIES.standard_relation,
+            ACTIVITIES.standard_type,
+            ACTIVITIES.standard_value,
+            ACTIVITIES.standard_units,
+            ASSAYS.assay_organism as strain,
+            ASSAYS.chembl_id as assay_id
+        FROM MOLECULE_DICTIONARY
+        JOIN ACTIVITIES ON MOLECULE_DICTIONARY.molregno == ACTIVITIES.molregno
+        JOIN ASSAYS ON ACTIVITIES.assay_id == ASSAYS.assay_id
+        WHERE
+            ASSAYS.assay_type == 'F'
+            and ACTIVITIES.standard_value is not null
+            and ACTIVITIES.standard_relation is not null
+            and ACTIVITIES.standard_relation = '='
+            and ACTIVITIES.standard_type = 'MIC'
     """
 
     assay_df = chembl_downloader.query(get_assays)
-    assay_df = assay_df[assay_df['assay_organism'].isin(bacterial_species)]
+    assay_df = assay_df[assay_df['strain'].isin(bacterial_species)]
+    assay_df.to_csv(f'../data/MIC/data_dump_{version}.tsv', sep='\t', index=False)
 
-    get_activity = """
-    SELECT
-        ACTIVITIES.activity_id,
-        ACTIVITIES.assay_id as id,
-        ACTIVITIES.standard_relation,
-        ACTIVITIES.standard_type,
-        ACTIVITIES.standard_value,
-        ACTIVITIES.standard_units,
-        ACTIVITIES.molregno as mol_id
-    FROM ACTIVITIES
-    """
-
-    act_df = chembl_downloader.query(get_activity)
-
-    data_df = pd.merge(assay_df, act_df, on='id', how='left')
-
-    get_compounds = """
-        SELECT
-            MOLECULE_DICTIONARY.molregno as mol_id,
-            MOLECULE_DICTIONARY.pref_name,
-            MOLECULE_DICTIONARY.max_phase
-        FROM MOLECULE_DICTIONARY
-        """
-
-    chem_df = chembl_downloader.query(get_compounds)
-
-    data_df = pd.merge(data_df, chem_df, on='mol_id', how='left')
-    data_df = data_df[data_df['standard_type'] == 'MIC']
-    data_df.to_csv(f'../data/MIC/data_dump_{version}.tsv', sep='\t', index=False)
+    # get_activity = """
+    # SELECT
+    #
+    # FROM ACTIVITIES
+    # WHERE and ACTIVITIES.standard_relation = '='
+    # """
+    #
+    # act_df = chembl_downloader.query(get_activity)
+    #
+    # data_df = pd.merge(assay_df, act_df, on='id', how='left')
+    #
+    # get_compounds = """
+    #     SELECT
+    #
+    #
+    #     FROM MOLECULE_DICTIONARY
+    #     """
+    #
+    # chem_df = chembl_downloader.query(get_compounds)
+    #
+    # data_df = pd.merge(data_df, chem_df, on='mol_id', how='left')
+    # data_df = data_df[data_df['standard_type'] == 'MIC']
+    # data_df.to_csv(f'../data/MIC/data_dump_{version}.tsv', sep='\t', index=False)
 
 
 if __name__ == '__main__':
